935dd8e48ca307139181340af3c14261ba579c97
Ahmed Şeref GÜNEYSU <serefguneysu@gmail.com>
2017-08-16 01:12:25 +0300

    ---

--- sensorfusion/pipelines/__main__.py
+++ sensorfusion/pipelines/__main__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding: utf-8
 import os
+import numpy as np
+import pandas as pd
 
 os.environ['DATA_DIR'] = '/home/ahmed/workspace/data/sensorlog'
 os.environ['DATA_FILENAME'] = 'f5GX_G5eT1Y_20170814T174804.txt'
@@ -8,18 +10,33 @@ os.environ['DATA_FILENAME'] = 'f5GX_G5eT1Y_20170814T174804.txt'
 DATA_DIR, DATA_FILENAME = os.environ['DATA_DIR'], os.environ['DATA_FILENAME']
 DATA_FILE = os.path.join(DATA_DIR, DATA_FILENAME)
 
-
 from sensorfusion.pipelines import readers
 from sensorfusion.pipelines import mechanics
 
-def main():
-    df = readers.sensor_data(DATA_FILE)
-    df = readers.clean(df,['_', 'alt'])
-    df = mechanics.t(df)
-    df = mechanics.latlon(df)
 
-    print(df.head(3))
-    # print(df.info())
+def main():
+    pd.DataFrame.drop = lambda this, columns: readers.clean(this, columns)
+    pd.DataFrame.t = lambda this: mechanics.t(this)
+    pd.DataFrame.latlon = lambda this: mechanics.latlon(this)
+    pd.DataFrame.dt = lambda this: mechanics.dt(this)
+    pd.DataFrame.dx = lambda this: mechanics.dx(this)
+    pd.DataFrame.v = lambda this: mechanics.v(this)
+    pd.DataFrame.normalize = lambda this: mechanics.normalize_nans(this)
+    pd.DataFrame.summary = lambda this: this.agg({'v_kmh': ['max', 'min', 'average']})
+
+    df = readers.sensor_data(DATA_FILE)
+
+    df = df \
+        .drop(columns=['_', 'alt']) \
+        .t() \
+        .latlon() \
+        .dt() \
+        .dx() \
+        .v() \
+        .normalize()
+
+    print(df.summary())
+
     return
 
 
--- sensorfusion/pipelines/mechanics.py
+++ sensorfusion/pipelines/mechanics.py
@@ -2,21 +2,51 @@
 # coding: utf-8
 
 import geopy.distance
+import numpy
+import pandas as pd
 
 
 def shift_column(df, _from, _to, default=None):
     default = df[_from][0] if default is None else default
-
     df[_to] = df[_from]
     df[_to][1:-1], df[_to][0] = df[_to][2:], default
     return df
 
 
 def t(df):
-    return shift_column(df, _from='t0', _to='t1')
+    return shift_column(df, _from='t0', _to='t1', default=df['t0'][1])
+
+
+def dt(df):
+    df['dt'] = df.t1 - df.t0
+    return df
+
+
+def distance(lat0, lon0, lat1, lon1):
+    return geopy.distance.vincenty(
+        (lat1, lon1),
+        (lat0, lon0)
+    ).meters
+
+
+def dx(df):
+    df['dx'] = tuple(map(lambda a, b, c, d: distance(a, b, c, d), df['lat0'], df['lon0'], df['lat1'], df['lon1']))
+    return df
+
+
+def v(df):
+    df['v_ms'] = 1000.0 * df['dx'] / df['dt']
+    df['v_kmh'] = 3.6 * df['v_ms']
+    return df
 
 
 def latlon(df):
-    df = shift_column(df, _from='lat0', _to='lat1', default=0)
-    df = shift_column(df, _from='lon0', _to='lon1', default=0)
+    df = shift_column(df, _from='lat0', _to='lat1', default=df['lat0'][0])
+    df = shift_column(df, _from='lon0', _to='lon1', default=df['lon0'][0])
+    return df
+
+
+def normalize_nans(df):
+    df = df[numpy.isfinite(df['v_kmh'])]
+    df = df[numpy.isfinite(df['v_ms'])]
     return df
